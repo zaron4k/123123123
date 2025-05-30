a516530cecee2f78c0ac5305fa6624225d0aac05
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-05-30 12:37:16 +0300

    Add files via upload

---
+++ ConsoleApp26/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Point <T>
+    {
+        private T x { get; set; }
+        private T y { get; set; }
+        
+        public Point (T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Output()
+        {
+        Console.WriteLine($"X = {x}\nY = {y}");
+        }
+    }
+}
---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> pointInt = new Point<int>(10, 20);
+            pointInt.Output();
+
+            Point<double> pointDouble = new Point<double>(3.14, 2.71);
+            pointDouble.Output();
+
+            Point<uint> pointString = new Point<uint>(12, 23);
+            pointString.Output();
+            Console.Read();
+        }
+    }
+}
